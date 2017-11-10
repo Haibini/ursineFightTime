bbd96102c40b06a099a0cea1891bec5a6f2e8080
Brendan <brendan.kelly94@gmail.com>
2017-11-09 17:50:32 -0800

    wgaufgruwhf
    fgsjkfg

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
 var mainState = {
     preload: function() {
-        this.game.load.image('player', 'assets/player.PNG');
+        this.game.load.spritesheet('player', 'assets/man.gif', 35, 42);
+        this.game.load.image('background', 'assets/scenario.jpg');
+        this.game.load.image('hp', 'assets/hearts.png');
+        this.game.load.image('enemy', 'assets/bear2.png');
         this.game.load.image('wall', 'assets/wall.PNG');
-        this.game.load.image('coin', 'assets/coin.PNG');
-        this.game.load.image('enemy', 'assets/lava.PNG');
     },
     
     create: function() {
-        this.game.stage.backgroundColor = '#3598db';
+//        this.game.load.image = 'assets/scenario.jpg';
+        this.background = this.game.add.tileSprite(0,0,850,470,'background');
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         game.world.enableBody = true;
         
         this.cursor = this.game.input.keyboard.createCursorKeys();
         
-        this.player = this.game.add.sprite(70, 100, 'player');
-        
-        this.player.body.gravity.y = 600;
+        this.player = this.game.add.sprite(50, 380, 'player');
         
         this.walls = this.game.add.group();
-        this.coins = this.game.add.group();
-        this.enemies = this.game.add.group();
         
         var level = [
             'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-            '!         !                               x',
-            '!                 o        o         o    x',
-            '!         o                     !         x',
-            '!                    x                    x',
-            '!     o   !    x     x        o      o    x',
-            'xxxxxxxxxxxxxxxx!!!!!xxxxxxxxxxxxxxxxxxxxxx',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'x                                         x',
+            'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         ];
         
         for (var i = 0; i < level.length; i++) {
             for (var j = 0; j < level[i].length; j++) {
 
                 if (level[i][j] == 'x') {
-                    var wall = game.add.sprite(30+20*j, 30+20*i, 'wall');
+                    var wall = game.add.sprite(20*j, 17.5*i, 'wall');
                     this.walls.add(wall);
                     wall.body.immovable = true; 
                 }
-
-                else if (level[i][j] == 'o') {
-                    var coin = game.add.sprite(30+20*j, 30+20*i, 'coin');
-                    this.coins.add(coin);
-                }
-
-                else if (level[i][j] == '!') {
-                    var enemy = game.add.sprite(30+20*j, 30+20*i, 'enemy');
-                    this.enemies.add(enemy);
-                }
             }
         }
+        
+//        this.player.body.gravity.y = 600;
+    
     },
     
     update: function() {
         this.game.physics.arcade.collide(this.player, this.walls);
 
-        this.game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this);
-
-        this.game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
+//        this.game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this);
+//
+//        this.game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
         
         if(this.cursor.left.isDown)
             this.player.body.velocity.x = -200;
@@ -71,11 +82,11 @@ var mainState = {
             this.player.body.velocity.y = -250;
     },
     
-    takeCoin: function(player, coin){
-        coin.kill();
-    },
+//    takeCoin: function(player, coin){
+//        coin.kill();
+//    },
 
-    restart: function() {
-        game.state.start('main');
-    },
+//    restart: function() {
+//        game.state.start('main');
+//    },
 }
