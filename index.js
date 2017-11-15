31cd934fae421a67d8055b8153c03d3f2ad10718
Brendan <brendan.kelly94@gmail.com>
2017-11-14 17:41:43 -0800

    SpriteSheet  Movement
    Achieve

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var mainState = {
     preload: function() {
-        this.game.load.spritesheet('player', 'assets/man.gif', 35, 42);
+        this.game.load.spritesheet('player', 'assets/man4.png', 88.57, 104.75);
         this.game.load.image('background', 'assets/scenario.jpg');
         this.game.load.image('hp', 'assets/hearts.png');
         this.game.load.image('enemy', 'assets/bear2.png');
         this.game.load.image('wall', 'assets/wall.PNG');
+        this.game.load.image('bullet','assets/bullet.png');
     },
     
     create: function() {
@@ -15,7 +16,22 @@ var mainState = {
         
         this.cursor = this.game.input.keyboard.createCursorKeys();
         
-        this.player = this.game.add.sprite(50, 380, 'player');
+        this.player = this.game.add.sprite(100, 100, 'player');
+        this.player.animations.add('right', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26], 20, true);
+        this.player.animations.add('left', [26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0], 20, true);
+        
+//        this.game.add.group();
+//        this.bullets.enableBody:true;
+//        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+//        this.bullets.createMultiple(30,'bullet');
+//        this.bullets.setAll('anchor.x', 0.5);
+//        this.bullets.setAll('anchor.y', 1);
+//        this.bullets.setAll('outOfBoundSkill', true);
+//        this.bullets.setAll('checkWorldBounds', true);
+//        
+//        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBARD);
+        
+//        this.player.animations.play("left")
         
         this.walls = this.game.add.group();
         
@@ -60,7 +76,7 @@ var mainState = {
             }
         }
         
-//        this.player.body.gravity.y = 600;
+        this.player.body.gravity.y = 600;
     
     },
     
@@ -71,15 +87,31 @@ var mainState = {
 //
 //        this.game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
         
-        if(this.cursor.left.isDown)
+        if(this.cursor.left.isDown){
             this.player.body.velocity.x = -200;
-        else if (this.cursor.right.isDown)
+            this.player.animations.play('left');
+        }
+        else if (this.cursor.right.isDown){
             this.player.body.velocity.x = 200;
-        else
+            this.player.animations.play('right');
+        }
+        else{
             this.player.body.velocity.x = 0;
+            this.player.animations.stop();
+
+            this.player.frame = 4;
+        }
+        
         
         if(this.cursor.up.isDown && this.player.body.touching.down)
+            {
             this.player.body.velocity.y = -250;
+        }
+        
+        
+//        if(fireButton.isDown){
+//            fireBullet();
+//        }
     },
     
 //    takeCoin: function(player, coin){
